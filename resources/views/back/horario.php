00b2001fa5e1bb34b94e54ad716e05c191060ee8
Pedro Eduardo <edu_452009@hotmail.com>
2023-10-30 23:23:30 -0500

    registro plantilla horario

--- resources/views/back/horario.php
+++ resources/views/back/horario.php
@@ -1,14 +1,25 @@
-<form action="/registrar_horario" method="post">
-    <div class="row">
-      <div class="col-md-2">
-        <label for="laboratorio">Laboratorio:</label>
-        <select name="laboratorio" id="laboratorio">
-          <option value="101">L-101</option>
-          <option value="202">L-202</option>
-          <option value="303">L-303</option>
-          <option value="404">L-404</option>
-        </select>
-      </div>
+<?php
+$hora_inicial = "7:00:00";
+$hora_final = "22:00:00";
+$intervalo = "00:45:00";
+
+$horarios = [];
+
+for ($hora = $hora_inicial; $hora <= $hora_final; $hora = date("H:i:s", strtotime($hora . " + $intervalo"))) {
+  $horarios[] = $hora;
+}
+?>
+
+<div class="container">
+  <form action="/registrar_horario" method="post">
+    <div class="form-group">
+      <label for="laboratorio">Laboratorio:</label>
+      <select name="laboratorio" id="laboratorio" class="form-control">
+        <option value="101">L-101</option>
+        <option value="202">L-202</option>
+        <option value="303">L-303</option>
+        <option value="404">L-404</option>
+      </select>
     </div>
     <table class="table table-bordered">
       <thead>
@@ -24,38 +35,20 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th>7:00</th>
-          <td><button type="button" class="btn btn-primary">Agregar</button></td>
-          <td><button type="button" class="btn btn-primary">Agregar</button></td>
-          <td><button type="button" class="btn btn-primary">Agregar</button></td>
-          <td><button type="button" class="btn btn-primary">Agregar</button></td>
-          <td><button type="button" class="btn btn-primary">Agregar</button></td>
-          <td><button type="button" class="btn btn-primary">Agregar</button></td>
-          <td><button type="button" class="btn btn-primary">Agregar</button></td>
-        </tr>
-        <tr>
-          <th>7:30</th>
-          <td><button type="button" class="btn btn-primary">Agregar</button></td>
-          <td><button type="button" class="btn btn-primary">Agregar</button></td>
-          <td><button type="button" class="btn btn-primary">Agregar</button></td>
-          <td><button type="button" class="btn btn-primary">Agregar</button></td>
-          <td><button type="button" class="btn btn-primary">Agregar</button></td>
-          <td><button type="button" class="btn btn-primary">Agregar</button></td>
-          <td><button type="button" class="btn btn-primary">Agregar</button></td>
-        </tr>
-        <tr>
-          <th>8:00</th>
-          <td><button type="button" class="btn btn-primary">Agregar</button></td>
-          <td><button type="button" class="btn btn-primary">Agregar</button></td>
-          <td><button type="button" class="btn btn-primary">Agregar</button></td>
-          <td><button type="button" class="btn btn-primary">Agregar</button></td>
-          <td><button type="button" class="btn btn-primary">Agregar</button></td>
-          <td><button type="button" class="btn btn-primary">Agregar</button></td>
-          <td><button type="button" class="btn btn-primary">Agregar</button></td>
-        </tr>
-        ...
+        <?php foreach ($horarios as $hora):?>
+          <tr>
+            <th><?php $hora|strftime('%I:%M %p')?></th>
+            <td><button type="button" class="btn btn-primary btn-block">Agregar</button></td>
+            <td><button type="button" class="btn btn-primary btn-block">Agregar</button></td>
+            <td><button type="button" class="btn btn-primary btn-block">Agregar</button></td>
+            <td><button type="button" class="btn btn-primary btn-block">Agregar</button></td>
+            <td><button type="button" class="btn btn-primary btn-block">Agregar</button></td>
+            <td><button type="button" class="btn btn-primary btn-block">Agregar</button></td>
+            <td><button type="button" class="btn btn-primary btn-block">Agregar</button></td>
+          </tr>
+          <?php endforeach; ?>
       </tbody>
     </table>
-    <button type="submit" class="btn btn-primary">Registrar horario</button>
+    <button type="submit" class="btn btn-primary btn-block">Registrar horario</button>
   </form>
+</div>
